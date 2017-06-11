431527ff78c942fffed35a3b8e1b612ed0273a58
Elieber Souza <elieber.souzans@gmail.com>
2017-06-10 21:02:51 -0400

    projeto sincronizado

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,88 @@ angular.module("bobotApp",[]).factory('GoogleMaps', function(){
       var mapOptions = {
         center: latLng,
         zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [
+{
+"featureType": "administrative",
+"elementType": "labels.text.fill",
+"stylers": [
+		{
+				"color": "#444444"
+		}
+]
+},
+{
+"featureType": "landscape",
+"elementType": "all",
+"stylers": [
+		{
+				"color": "#f2f2f2"
+		}
+]
+},
+{
+"featureType": "poi",
+"elementType": "all",
+"stylers": [
+		{
+				"visibility": "off"
+		}
+]
+},
+{
+"featureType": "road",
+"elementType": "all",
+"stylers": [
+		{
+				"saturation": -100
+		},
+		{
+				"lightness": 45
+		}
+]
+},
+{
+"featureType": "road.highway",
+"elementType": "all",
+"stylers": [
+		{
+				"visibility": "simplified"
+		}
+]
+},
+{
+"featureType": "road.arterial",
+"elementType": "labels.icon",
+"stylers": [
+		{
+				"visibility": "off"
+		}
+]
+},
+{
+"featureType": "transit",
+"elementType": "all",
+"stylers": [
+		{
+				"visibility": "off"
+		}
+]
+},
+{
+"featureType": "water",
+"elementType": "all",
+"stylers": [
+		{
+				"color": "#46bcec"
+		},
+		{
+				"visibility": "on"
+		}
+]
+}
+]
+			
       };
       
  
