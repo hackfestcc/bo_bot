9eef8d2fd4011af70fdd8ff1b073a87e6654edc1
Elieber Souza <elieber.souzans@gmail.com>
2017-06-11 10:36:43 -0400

    adicionado descricao e motivo

--- scripts/app.js
+++ scripts/app.js
@@ -127,8 +127,7 @@ angular.module("bobotApp",[]).factory('GoogleMaps', function($http){
                     '<div class="iw-title">'+record.tipo+'</div>' +
                     '<div class="iw-content">' +
                       '<div class="iw-subTitle">Período da ocorrência: '+record.turno+'</div>' +
-                      '<img src="http://maps.marnoto.com/en/5wayscustomizeinfowindow/images/vistalegre.jpg" alt="Porcelain Factory of Vista Alegre" height="115" width="83">' +
-                      '<p>Founded in 1824, the Porcelain Factory of Vista Alegre was the first industrial unit dedicated to porcelain production in Portugal. For the foundation and success of this risky industrial development was crucial the spirit of persistence of its founder, José Ferreira Pinto Basto. Leading figure in Portuguese society of the nineteenth century farm owner, daring dealer, wisely incorporated the liberal ideas of the century, having become "the first example of free enterprise" in Portugal.</p>' +
+                      '<p>'+record.motivo+'</p>' +
                       '<div class="iw-subTitle">Contacts</div>' +
                       '<p>VISTA ALEGRE ATLANTIS, SA<br>3830-292 Ílhavo - Portugal<br>'+
                       '<br>Phone. +351 234 320 600<br>e-mail: geral@vaa.pt<br>www: www.myvistaalegre.com</p>'+
